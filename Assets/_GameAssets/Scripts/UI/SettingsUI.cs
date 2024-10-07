0ccccd9c6ec2210dbc40601438021cc87d4c62f4
Tayfun Yılmaz <tyfnylmz201974@gmail.com>
2024-10-07 09:09:32 +0300

    v0.24 Environment Changes

---
+++ Assets/_GameAssets/Scripts/UI/SettingsUI.cs
@@ -0,0 +1,82 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsUI : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject _settingsPopupObject;
+
+    [Header("Buttons")]
+    [SerializeField] private Button _settingsButton;
+    [SerializeField] private Button _musicButton;
+    [SerializeField] private Button _soundButton;
+    [SerializeField] private Button _resumeButton;
+    [SerializeField] private Button _mainMenuButton;
+
+    [Header("Sprites")]
+    [SerializeField] private Sprite _musicActiveSprite;
+    [SerializeField] private Sprite _musicPassiveSprite;
+    [SerializeField] private Sprite _soundActiveSprite;
+    [SerializeField] private Sprite _soundPassiveSprite;
+
+    [Header("Settings")]
+    [SerializeField] private float _scaleDuration;
+
+    private bool _isMusicActive = true;
+    private bool _isSoundActive = true;
+
+    private void Awake() 
+    {
+        _settingsPopupObject.transform.localScale = Vector3.zero;
+
+        _settingsButton.onClick.AddListener(OnSettingsButtonClicked);
+        _musicButton.onClick.AddListener(OnMusicButtonClicked);
+        _soundButton.onClick.AddListener(OnSoundButtonClicked);
+        _resumeButton.onClick.AddListener(OnResumeButtonClicked);
+        _mainMenuButton.onClick.AddListener(OnMainMenuButtonClicked);
+    }
+
+    private void OnSettingsButtonClicked()
+    {
+        _settingsPopupObject.SetActive(true);
+        _settingsPopupObject.transform.DOScale(1f, _scaleDuration).SetEase(Ease.OutBack);
+        //TODO: Pause the Game.
+    }
+
+    private void OnResumeButtonClicked()
+    {
+        _settingsPopupObject.transform.DOScale(0f, _scaleDuration).SetEase(Ease.OutExpo).OnComplete(() =>
+        {
+            _settingsPopupObject.SetActive(false);
+            //TODO: Resume the Game.
+        });
+    }
+
+    private void OnMusicButtonClicked()
+    {
+        _isMusicActive = !_isMusicActive;
+        _musicButton.image.sprite = _isMusicActive ? _musicActiveSprite : _musicPassiveSprite;
+    }
+
+    private void OnSoundButtonClicked()
+    {
+        _isSoundActive = !_isSoundActive;
+        _soundButton.image.sprite = _isSoundActive ? _soundActiveSprite : _soundPassiveSprite;
+    }
+
+    private void OnMainMenuButtonClicked()
+    {
+        //TODO: Go to Main Menu.
+    }
+
+    private void OnDestroy() 
+    {
+        _settingsButton.onClick.RemoveListener(OnSettingsButtonClicked);
+        _musicButton.onClick.RemoveListener(OnMusicButtonClicked);
+        _soundButton.onClick.RemoveListener(OnSoundButtonClicked);
+        _resumeButton.onClick.RemoveListener(OnResumeButtonClicked);
+        _mainMenuButton.onClick.RemoveListener(OnMainMenuButtonClicked);    
+    }
+
+}
